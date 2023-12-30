b4e67e3c56717e53c4f01a1fb44d8a1d7b32f333
salaudeen22 <salaudeensalu@gmail.com>
2023-12-30 20:55:26 +0530

    header update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./icons_assets/Logo.svg";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [menuopen, setmenuopen] = useState(false);
+  const togglemenu = () => {
+    setmenuopen(!menuopen);
+    console.log("Menu open state:", menuopen);
+  };
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+    <header>
+        <nav className={`navbar ${menuopen ? "open" : ""}`}>
+          <a href="/">
+            <img src={logo} alt="logo" />
+          </a>
+          <div className="menu-icon" onClick={togglemenu}>
+            <div className="bar"></div>
+            <div className="bar"></div>
+            <div className="bar"></div>
+          </div>
+          <ul className={`nav-links ${menuopen ? "visible" : ""}`}>
+            <li>
+            <a href="/"> Home</a>
+          </li>
+          <li>
+            <a href="/">About</a>
+          </li>
+          <li>
+            <a href="/">Services</a>
+          </li>
+          <li>
+            <a href="/"> Menu</a>
+          </li>
+          <li>
+            <a href="/">Reservation</a>
+          </li>
+          <li>
+            <a href="/">order onlinr</a>
+          </li>
+          <li>
+            <a href="/">Login</a>
+          </li>
+        </ul>
+      </nav>
       </header>
-    </div>
+      <main></main>
+      <footer></footer>
+    </>
   );
 }
 
